748c40fa50b3a79391758a4c9c69ac08774a3e6a
RpM-Vic <victorurielmartinezperez@gmail.com>
2026-04-10 14:11:37 -0600

    took away rate limiter

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ import { chatRoutes } from './routes/chat-routes.js';
 import { embedBook } from './routes/embed-routes.js';
 import { pages } from './routes/pages-routes.js';
 import { paymentsRouter } from './routes/payments-routes.js';
-import { rateLimiter, authRateLimit, supportRateLimit } from './middlewares/rate-limit.js';
+// import { rateLimiter, authRateLimit, supportRateLimit } from './middlewares/rate-limit.js';
 import { strictJson } from './middlewares/size-limiter.js';
-import { stripeRouter } from './webhooks.ts/stripe.js';
+import { stripeRouter } from './webhooks/stripe.js';
 import { testConnection } from './DB/dbConnection.js'
 import { validateSession } from './middlewares/cookies.js';
 import { supportRouter } from './routes/support-routes.js';
@@ -28,12 +28,21 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use('/',pages)
-app.use('/api/auth',[strictJson,authRateLimit],authRouter)
-app.use('/api/embedBook',[validateSession,rateLimiter],embedBook)
-app.use('/api/books',[strictJson,rateLimiter,validateSession],booksRouter)
+app.use('/api/auth',[strictJson],authRouter)
+app.use('/api/embedBook',[validateSession],embedBook)
+app.use('/api/books',[strictJson,validateSession],booksRouter)
 app.use('/api/purchase',strictJson,paymentsRouter)
 app.use('/api/chat',validateSession,chatRoutes)
-app.use('/api/support',supportRateLimit,supportRouter)
+app.use('/api/support',supportRouter)
+
+
+// app.use('/',pages)
+// app.use('/api/auth',[strictJson,authRateLimit],authRouter)
+// app.use('/api/embedBook',[validateSession,rateLimiter],embedBook)
+// app.use('/api/books',[strictJson,rateLimiter,validateSession],booksRouter)
+// app.use('/api/purchase',strictJson,paymentsRouter)
+// app.use('/api/chat',validateSession,chatRoutes)
+// app.use('/api/support',supportRateLimit,supportRouter)
 
 app.use('*foo',pages)
 
--- src/middlewares/rate-limit.ts
+++ src/middlewares/rate-limit.ts
@@ -1,20 +1,20 @@
 import rateLimit from 'express-rate-limit';
 
 export const rateLimiter = rateLimit({
-    windowMs:  60 * 1000, // 1 minutes
-    max: 120, 
-    message: 'Server is busy, try again later',
+  windowMs: 60 * 1000, // 1 minutes
+  max: 120,
+  message: 'Server is busy, try again later',
 });
 
-// Stricter rate limiter for login 
+// Stricter rate limiter for login
 export const authRateLimit = rateLimit({
-  windowMs: 6*60 * 60 * 1000, // 6 hours
-  max: 30, 
+  windowMs: 6 * 60 * 60 * 1000, // 6 hours
+  max: 30,
   message: 'Server is busy, try again later',
 });
 
 export const supportRateLimit = rateLimit({
-  windowMs: 12*60 * 60 * 1000, // 6 hours
-  max: 2, 
+  windowMs: 12 * 60 * 60 * 1000, // 6 hours
+  max: 2,
   message: 'We are reviewing your case',
 });
