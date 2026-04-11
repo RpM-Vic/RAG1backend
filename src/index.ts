4c748b3bc9a2096d83374de433d4b7b54f5eeb61
RpM-Vic <victorurielmartinezperez@gmail.com>
2026-04-11 16:12:50 -0600

    feature: added 2 factor to frontend

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { chatRoutes } from './routes/chat-routes.js';
 import { embedBook } from './routes/embed-routes.js';
 import { pages } from './routes/pages-routes.js';
 import { paymentsRouter } from './routes/payments-routes.js';
-// import { rateLimiter, authRateLimit, supportRateLimit } from './middlewares/rate-limit.js';
+import { rateLimiter, authRateLimit, supportRateLimit } from './middlewares/rate-limit.js';
 import { strictJson } from './middlewares/size-limiter.js';
 import { stripeRouter } from './webhooks/stripe.js';
 import { testConnection } from './DB/dbConnection.js'
@@ -27,25 +27,24 @@ app.use('/api/webhook/stripe', stripeRouter); // ⬅️ Dedicated raw-body befor
 app.use(cookieParser());
 app.use(express.json());
 
-app.use('/',pages)
-app.use('/api/auth',[strictJson],authRouter)
-app.use('/api/embedBook',[validateSession],embedBook)
-app.use('/api/books',[strictJson,validateSession],booksRouter)
-app.use('/api/purchase',strictJson,paymentsRouter)
-app.use('/api/chat',validateSession,chatRoutes)
-app.use('/api/support',supportRouter)
-
-
 // app.use('/',pages)
-// app.use('/api/auth',[strictJson,authRateLimit],authRouter)
-// app.use('/api/embedBook',[validateSession,rateLimiter],embedBook)
-// app.use('/api/books',[strictJson,rateLimiter,validateSession],booksRouter)
+// app.use('/api/auth',[strictJson],authRouter)
+// app.use('/api/embedBook',[validateSession],embedBook)
+// app.use('/api/books',[strictJson,validateSession],booksRouter)
 // app.use('/api/purchase',strictJson,paymentsRouter)
 // app.use('/api/chat',validateSession,chatRoutes)
-// app.use('/api/support',supportRateLimit,supportRouter)
+// app.use('/api/support',supportRouter)
+
+
+app.use('/',pages)
+app.use('/api/auth',[strictJson,authRateLimit],authRouter)
+app.use('/api/embedBook',[validateSession,rateLimiter],embedBook)
+app.use('/api/books',[strictJson,rateLimiter,validateSession],booksRouter)
+app.use('/api/purchase',strictJson,paymentsRouter)
+app.use('/api/chat',validateSession,chatRoutes)
+app.use('/api/support',supportRateLimit,supportRouter)
 
 app.use('*foo',pages)
-
 
 export default app;
 
